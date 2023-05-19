a12c3a4f7728733e6105019d31194353062833b8
Alexandernevsk <Alexandertorreman@gmail.com>
2023-05-19 12:44:36 +0200

    Updated version

--- src/Week1/Day1/Day31/Grid.java
+++ src/Week1/Day1/Day31/Grid.java
@@ -1,8 +1,8 @@
 package Week1.Day1.Day31;
 
 import java.util.ArrayList;
-import java.util.List;
-import java.util.stream.Stream;
+import java.util.InputMismatchException;
+import java.util.Scanner;
 
 public class Grid {
     private static final String IN_BETWEEN_GRID = "\n-+-+-\n";
@@ -44,16 +44,69 @@ public class Grid {
             System.out.println("not a valid move");
         }
     }
-    public boolean checkWin(int player){
+    public boolean checkWin(int player) {
+        int[][] winningCombinations = {
+                {0, 1, 2}, {3, 4, 5}, {6, 7, 8}, // rows
+                {0, 3, 6}, {1, 4, 7}, {2, 5, 8}, // columns
+                {0, 4, 8}, {2, 4, 6} // diagonals
+        };
+
+        for (int[] combination : winningCombinations) {
+            if (comparingElements(combination[0], combination[1], combination[2], player)) {
+                return true;
+            }
+        }
+
         return false;
     }
-
-    public ArrayList<DotCross> getWinningCombinations(){
-     return null;
+    public boolean comparingElements(int x, int y, int z, int player){
+      if(_grid.get(x).equals(players[player]))
+        if((_grid.get(x).equals(_grid.get(y)))){
+            return _grid.get(y).equals(_grid.get(z));
+        }
+      return false;
     }
 
-    public static void main(String[] args) {
-        Grid grid = new Grid();
+    public boolean checkDraw(){
+        return !_grid.contains(empty);
+    }
 
+    public  void runGrid(Grid grid){
+       boolean endGame = false;
+       while(!endGame){
+           System.out.println(grid);
+           System.out.println("Player 1 your turn");
+           setMove(inputScanner()-1,0);
+           System.out.println(grid);
+           System.out.println("Player 2 your turn");
+           setMove(inputScanner()-1,1);
+           System.out.println(grid);
+           if(checkWin(0)){
+               System.out.println("player 1 wins!");
+               endGame = checkWin(0);
+           }else if(checkWin(1)){
+               System.out.println("player 2 wins");
+               endGame = checkWin(1);
+           }else {
+               System.out.println("its a draw");
+               endGame = checkDraw();
+           }
+       }
+    }
+
+    public int inputScanner(){
+        Scanner scanner = new Scanner(System.in);
+        int move =0;
+        try{
+            move = scanner.nextInt();
+            if(move < 1 || move > 9){
+                System.out.println("not a valid number");
+                inputScanner();
+            }
+        } catch (InputMismatchException e){
+            System.out.println("not a valid input");
+            inputScanner();
+        }
+        return move;
     }
 }
---
+++ src/Week1/Day1/Day31/runTicTacToe.java
@@ -0,0 +1,8 @@
+package Week1.Day1.Day31;
+
+public class runTicTacToe {
+    public static void main(String[] args) {
+        Grid grid = new Grid();
+        grid.runGrid(grid);
+    }
+}
